cc5482236c20f038fa32c7c9939c6c19934692ff
msarefin <mohammed.arefin@gmail.com>
2017-09-09 00:21:44 -0400

    update

--- Training for SQL Server 2012/SQL Server 2012 Training/SQL Server 2012 Training/Chapter 1.sql
+++ Training for SQL Server 2012/SQL Server 2012 Training/SQL Server 2012 Training/Chapter 1.sql
@@ -3,4 +3,32 @@ go
 
 --Chapter 1
 
-select * from 
+select country 
+from hr.Employees; 
+
+----------
+
+select distinct country from hr.Employees; 
+
+
+--------------
+
+
+select empid, lastname from hr.Employees; 
+
+------------
+
+
+select empid, lastname from hr.Employees order by empid; 
+
+------------------------
+
+select empid, lastname from hr.employees order by 1; 
+
+---------------
+
+
+select empid, firstname+' '+lastname as FullName 
+from hr.Employees; 
+
+
