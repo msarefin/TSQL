760ffd26724efff356c14d7d8fe9c5144e586513
msarefin <mohammed.arefin@gmail.com>
2017-10-11 19:14:26 -0400

    updated

--- Practice/Working with Nulls in the WHERE clasuse.sql
+++ Practice/Working with Nulls in the WHERE clasuse.sql
@@ -31,4 +31,24 @@ select e.empid, e.firstname, e.lastname, e.city, e.region, e.country
 from hr.Employees as e where e.region is null; 
 
 -- NUll is treated by SQL as an empty null mark. Null is not equal no Null 
+use TSQL2012; 
 
+go 
+
+select orderid, orderdate, custid, empid
+from sales.orders 
+where shippeddate = null;
+
+
+select orderid, orderdate, custid, empid
+from sales.orders where shippeddate is null; 
+
+
+select orderid, orderdate, custid, empid
+from Sales.Orders
+where orderdate between '20080211' and '20080212 23:59:59.999' ; 
+
+
+select orderid, orderdate, custid, empid
+from sales.orders 
+where orderdate >= '20080211' and orderdate < '20080213'
