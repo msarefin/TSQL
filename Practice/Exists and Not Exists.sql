8b1bc77295b200df712f84bd790e20e81149428c
Sunny <mohammed.arefin@gmail.com>
2017-10-21 17:24:15 -0400

    update

--- Practice/Exists and Not Exists.sql
+++ Practice/Exists and Not Exists.sql
@@ -8,15 +8,3 @@ from sales.Customers as c where not exists (select * from sales.Orders as o wher
 select custid, companyname
 from sales.Customers as c where exists(select * from sales.orders as o where o.custid= c.custid and o.orderid='20070212');
 
--- Derived table 
-
-select * 
-from (
-select ROW_NUMBER() over(partition by categoryid order by unitprice, productid) as rownum, 
-categoryid, 
-productid,
-productname, 
-unitprice 
-from Production.Products) as d
-where rownum <=2
-;
