2b650e85d589e217811b3f4746ab3a549359360e
Sunny <mohammed.arefin@gmail.com>
2017-11-27 15:18:35 -0500

    Dynaimc Pivot operator

---
+++ Practice/DynamicPivot.sql
@@ -0,0 +1,30 @@
+use TSQL2012; 
+go 
+
+declare @shipperid nvarchar(max) ='';
+
+
+select  @shipperid += quotename(d.shipperid) +',' 
+from Sales.Orders as d
+group by d.shipperid
+
+set @shipperid = LEFT(@shipperid, LEN(@shipperid)-1)
+
+
+print @shipperid;
+
+print 'This is working !'
+
+declare @sql nvarchar(max);
+
+set @sql='
+with pt as 
+(
+select d.empid, d.shipperid,d.freight
+from Sales.Orders as d 
+)
+select empid ,'+@shipperid+' from pt
+pivot (sum(freight) for shipperid in ('+@shipperid+'))as p;
+'
+print @sql
+exec sp_executesql @sql;
--- Practice/pivot and unvivot table.sql
+++ Practice/pivot and unvivot table.sql
@@ -36,3 +36,34 @@ select custid, [1],[2],[3] from pt pivot (count(freight) for shipperid in ([1],[
 order by custid
 ;
 
+------------------------Dynamic Pivot table----------------
+
+
+use TSQL2012; 
+go 
+
+declare @shipperid nvarchar(max) =''
+
+
+select  @shipperid += quotename(d.shipperid) +',' 
+from Sales.Orders as d
+group by d.shipperid
+
+set @shipperid = LEFT(@shipperid, LEN(@shipperid)-1)
+
+--print @shipperid
+declare @sql varchar(max) 
+set @sql=
+'
+with pt as 
+(
+select custid,shipperid, freight 
+from Sales.Orders
+)
+
+select custid,freight from pt
+pivot(sum(freight) for shipperid in ('+@shipperid+')) as pvt
+'
+
+exec sp_executesql @Sql;
+
