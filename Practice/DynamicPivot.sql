23d542840ec8dd41f493adb59ed7176dadddb1da
Sunny <mohammed.arefin@gmail.com>
2017-12-06 20:03:49 -0500

    dynamic pivot table with derived table

--- Practice/DynamicPivot.sql
+++ Practice/DynamicPivot.sql
@@ -28,3 +28,37 @@ pivot (sum(freight) for shipperid in ('+@shipperid+'))as p;
 '
 print @sql
 exec sp_executesql @sql;
+
+
+--- Dynmaic pivot table using derived table
+
+use TSQL2012;
+go 
+
+declare @country as nvarchar(max) = '';
+
+select @country += quotename(e.country)+','
+from hr.Employees as e 
+group by e.country
+
+
+
+set @country= LEFT(@country, LEN(@country)-1)
+
+print @country;
+
+declare @sql as nvarchar(max)
+
+set @sql=
+'
+select name,'+@country+'
+from
+(select
+	CONCAT(e.firstname,'' '',e.lastname) as [name], DATEDIFF(year,e.birthdate, SYSDATETIME()) as [Age], e.country
+from hr.Employees as e
+) as d 
+pivot(max(age) for country in ('+@country+')) as pvt
+order by USA desc
+'
+
+exec sp_executesql @sql;
