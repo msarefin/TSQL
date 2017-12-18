1645a863f5eef2c7beb6e1d335ad5a0966d00d04
Sunny <mohammed.arefin@gmail.com>
2017-12-18 03:29:19 -0500

    window offset function lead and lag

--- Practice/Window Offset function.sql
+++ Practice/Window Offset function.sql
@@ -2,9 +2,12 @@ use TSQL2012;
 go 
 
 select custid, orderid, orderdate, val, 
-LAG(val) over(partition by custid order by orderdate, orderid) as prev_val,
-LEAD(val) over(partition by custid order by orderdate, orderid) as next_val
-from Sales.OrderValues
+LAG(val) over(partition by custid order by orderdate, orderid) as prev_val, -- Value from the previous row
+LEAD(val) over(partition by custid order by orderdate, orderid) as next_val, -- Value form the next row
+FIRST_VALUE(val) over (partition by custid order by orderdate, orderid rows between unbounded preceding and current row) first_val, --First value in the set 
+LAST_VALUE(val) over (partition by custid order by orderdate, orderid rows between unbounded preceding and unbounded following) last_val -- Last value in the set
+from Sales.OrderValues 
+
 ; 
 
 
