2e19448da9cf320caf5f1bd77a36e4e98cf5b2d4
sunny <mohammed.arefin@gmail.com>
2018-01-04 22:44:02 -0500

    update

--- Practice/Querying Full text data.sql
+++ Practice/Querying Full text data.sql
@@ -1,7 +1,15 @@
 -- Querying full text data
 use TSQL2012;
 go 
+
+
+-- Check if full text filter is instaled or not?
 select serverproperty('Isfulltextinstalled') as 'Is full text search enable?'
 
+--Chekc which filters are installed?
 
+exec sys.sp_help_fulltext_system_components 'filter';
+
+select document_type, path 
+from sys.fulltext_document_types
 
