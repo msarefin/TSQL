8cb93df9f2e40d20b1fc7c74f3706de1ead3ace2
Sunny <mohammed.arefin@gmail.com>
2018-01-18 13:55:58 -0500

    update

--- Practice/Querying Full text data.sql
+++ Practice/Querying Full text data.sql
@@ -6,10 +6,17 @@ go
 -- Check if full text filter is instaled or not?
 select serverproperty('Isfulltextinstalled') as 'Is full text search enable?'
 
---Chekc which filters are installed?
+--Check which filters are installed?
 
 exec sys.sp_help_fulltext_system_components 'filter';
 
 select document_type, path 
 from sys.fulltext_document_types
+
+exec sys.sp_fulltext_service 'load_os_resources',1; 
+
+
+-- Check which languages are supported in SQL Server 
+select lcid, name
+from sys.fulltext_languages order by name; 
 
